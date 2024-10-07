eee36cbbbf6c19c15e85e036f02d880afdaed1ad
CybErr404 <wattsm6@go.stockton.edu>
2024-10-07 14:16:47 -0400

    Added more method tests

--- src/TestMiaDaisyChainConnector.java
+++ src/TestMiaDaisyChainConnector.java
@@ -10,5 +10,21 @@ public class TestMiaDaisyChainConnector {
         }
         System.out.println(daisyChain);
         System.out.println(daisyChain.size());
+        daisyChain.replace(2, 100);
+        System.out.println(daisyChain);
+        daisyChain.addEnd(19);
+        System.out.println(daisyChain);
+        daisyChain.addAtIndex(3, 1000);
+        System.out.println(daisyChain);
+        //System.out.println(daisyChain.contains(19));
+        //System.out.println(daisyChain.contains(20));
+        daisyChain.removeFront();
+        System.out.println(daisyChain);
+        daisyChain.removeEnd();
+        System.out.println(daisyChain);
+        daisyChain.removeAtIndex(1);
+        System.out.println(daisyChain);
+        daisyChain.clear();
+        System.out.println(daisyChain);
     }
 }
