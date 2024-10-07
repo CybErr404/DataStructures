a3ecf22ae89e29274096f8a309ba065a595eb3d7
CybErr404 <wattsm6@go.stockton.edu>
2024-10-07 12:50:56 -0400

    Added tester

---
+++ src/TestMiaDaisyChainConnector.java
@@ -0,0 +1,14 @@
+//DONE BEFORE ANYTHING ELSE:
+//Write a tester.
+//Call add method for 6 elements.
+//Print out results.
+public class TestMiaDaisyChainConnector {
+    public static void main(String[] args) {
+        MiaDaisyChainConnector daisyChain = new MiaDaisyChainConnector();
+        for(int i = 0; i < 6; i++) {
+            daisyChain.addFront(i);
+        }
+        System.out.println(daisyChain);
+        System.out.println(daisyChain.size());
+    }
+}
