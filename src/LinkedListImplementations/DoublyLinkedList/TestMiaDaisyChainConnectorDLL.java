be9bc9350c2fa6e4b4831b615166f1fe4e72a0be
CybErr404 <wattsm6@go.stockton.edu>
2024-10-09 17:36:08 -0400

    Finished Doubly LinkedList tester

--- src/LinkedListImplementations/DoublyLinkedList/TestMiaDaisyChainConnectorDLL.java
+++ src/LinkedListImplementations/DoublyLinkedList/TestMiaDaisyChainConnectorDLL.java
@@ -1,5 +1,45 @@
 package LinkedListImplementations.DoublyLinkedList;
 
 public class TestMiaDaisyChainConnectorDLL {
+    public static void main(String[] args) {
+        MiaDaisyChainConnectorDLL daisyChain = new MiaDaisyChainConnectorDLL();
 
+        for(int i = 1; i <= 6; i++) {
+            daisyChain.addFront(i);
+        }
+
+        System.out.println("Daisy chain after adding numbers 1-6: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.replace(2, 100);
+        System.out.println("Daisy chain after replacing the number 2 with 100: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.addEnd(19);
+        System.out.println("Daisy chain after adding 19 to the end: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.addAtIndex(3, 1000);
+        System.out.println("Daisy chain after adding 1000 in position 3: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        System.out.println("Daisy chain contains 3: \n" + daisyChain.contains(3));
+        System.out.println("Daisy chain contains 20: \n" + daisyChain.contains(20) + "\n");
+
+        daisyChain.removeFront();
+        System.out.println("Daisy chain after removing the element at the beginning: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.removeEnd();
+        System.out.println("Daisy chain after removing the element at the end: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.removeAtIndex(1);
+        System.out.println("Daisy chain after removing the item in position 1: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        daisyChain.clear();
+        System.out.println("Cleared daisy chain: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+    }
 }
