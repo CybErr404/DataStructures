c8831eeb62b5996fb0883f5e8dcd41bf622d203b
CybErr404 <wattsm6@go.stockton.edu>
2024-10-09 17:37:48 -0400

    Edited Singularly LinkedList tester

--- src/LinkedListImplementations/SingularlyLinkedList/TestMiaDaisyChainConnectorSLL.java
+++ src/LinkedListImplementations/SingularlyLinkedList/TestMiaDaisyChainConnectorSLL.java
@@ -3,26 +3,43 @@ package LinkedListImplementations.SingularlyLinkedList;
 public class TestMiaDaisyChainConnectorSLL {
     public static void main(String[] args) {
         MiaDaisyChainConnectorSLL daisyChain = new MiaDaisyChainConnectorSLL();
+
         for(int i = 1; i <= 6; i++) {
             daisyChain.addFront(i);
         }
-        System.out.println("Daisy chain after adding 1-6: " + daisyChain);
-        System.out.println("Current size: " + daisyChain.size());
+
+        System.out.println("Daisy chain after adding numbers 1-6: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.replace(2, 100);
-        System.out.println("Daisy chain after replacing 2 with 100: " + daisyChain);
+        System.out.println("Daisy chain after replacing the number 2 with 100: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.addEnd(19);
-        System.out.println("Daisy chain after adding 19 to the end: " + daisyChain);
+        System.out.println("Daisy chain after adding 19 to the end: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.addAtIndex(3, 1000);
-        System.out.println("Daisy chain after adding 1000 in position 3: " + daisyChain);
-        System.out.println("Chain contains 3: " + daisyChain.contains(3));
-        System.out.println("Chain contains 20: " + daisyChain.contains(20));
+        System.out.println("Daisy chain after adding 1000 in position 3: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
+        System.out.println("Daisy chain contains 3: \n" + daisyChain.contains(3));
+        System.out.println("Daisy chain contains 20: \n" + daisyChain.contains(20) + "\n");
+
         daisyChain.removeFront();
-        System.out.println("Daisy chain after removing the element at the beginning: " + daisyChain);
+        System.out.println("Daisy chain after removing the element at the beginning: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.removeEnd();
-        System.out.println("Daisy chain after removing the element at the end: " + daisyChain);
+        System.out.println("Daisy chain after removing the element at the end: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.removeAtIndex(1);
-        System.out.println("Daisy chain after removing the item in position 1: " + daisyChain);
+        System.out.println("Daisy chain after removing the item in position 1: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
+
         daisyChain.clear();
-        System.out.println("Cleared daisy chain: " + daisyChain);
+        System.out.println("Cleared daisy chain: \n" + daisyChain);
+        System.out.println("Current size: " + daisyChain.size() + "\n");
     }
 }
