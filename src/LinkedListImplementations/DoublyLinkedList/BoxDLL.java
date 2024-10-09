d7a0eac09254347d8b6488c5dd0e8921fd8647c8
CybErr404 <wattsm6@go.stockton.edu>
2024-10-09 16:47:53 -0400

    Finished BoxDLL class

--- src/LinkedListImplementations/DoublyLinkedList/BoxDLL.java
+++ src/LinkedListImplementations/DoublyLinkedList/BoxDLL.java
@@ -1,5 +1,28 @@
 package LinkedListImplementations.DoublyLinkedList;
 
 public class BoxDLL {
-    
+    private BoxDLL next;
+    private BoxDLL previous;
+    private int data;
+
+    public BoxDLL(BoxDLL inputNext, BoxDLL inputPrevious, int inputData) {
+        next = inputNext;
+        previous = inputPrevious;
+        data = inputData;
+    }
+
+    public void setNext(BoxDLL newNext) {
+        next = newNext;
+    }
+    public BoxDLL getNext() {
+        return next;
+    }
+    public void setPrevious(BoxDLL newPrevious) { previous = newPrevious; }
+    public BoxDLL getPrevious() { return previous; }
+    public void setData(int newData) {
+        data = newData;
+    }
+    public int getData() {
+        return data;
+    }
 }
