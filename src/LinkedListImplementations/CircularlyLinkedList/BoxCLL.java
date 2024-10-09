90d3d89d503d5c5cd31cc5c59f9c24fc9d30ee98
CybErr404 <wattsm6@go.stockton.edu>
2024-10-09 17:40:13 -0400

    Added Circularly LinkedList box class

--- src/LinkedListImplementations/CircularlyLinkedList/BoxCLL.java
+++ src/LinkedListImplementations/CircularlyLinkedList/BoxCLL.java
@@ -1,5 +1,28 @@
 package LinkedListImplementations.CircularlyLinkedList;
 
 public class BoxCLL {
+    private BoxCLL next;
+    private BoxCLL previous;
+    private int data;
 
+    public BoxCLL(BoxCLL inputNext, BoxCLL inputPrevious, int inputData) {
+        next = inputNext;
+        previous = inputPrevious;
+        data = inputData;
+    }
+
+    public void setNext(BoxCLL newNext) {
+        next = newNext;
+    }
+    public BoxCLL getNext() {
+        return next;
+    }
+    public void setPrevious(BoxCLL newPrevious) { previous = newPrevious; }
+    public BoxCLL getPrevious() { return previous; }
+    public void setData(int newData) {
+        data = newData;
+    }
+    public int getData() {
+        return data;
+    }
 }
