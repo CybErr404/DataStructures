bf27094dceaefdea743cba7e06adcd5d3a25b50e
CybErr404 <wattsm6@go.stockton.edu>
2024-10-14 13:26:46 -0400

    Added Item class

---
+++ src/Project1/Item.java
@@ -0,0 +1,20 @@
+package Project1;
+
+public abstract class Item implements Interactable {
+    private String itemName;
+    private int value;
+
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String newItemName) {
+        itemName = newItemName;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int newValue) {
+        value = newValue;
+    }
+}
