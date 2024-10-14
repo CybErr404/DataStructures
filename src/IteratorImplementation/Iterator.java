a4f6d72418c5417ca1997e69bb22188d8606fc60
CybErr404 <wattsm6@go.stockton.edu>
2024-10-14 16:49:18 -0400

    Created Iterator interface

---
+++ src/IteratorImplementation/Iterator.java
@@ -0,0 +1,24 @@
+package IteratorImplementation;
+
+public interface Iterator {
+    //Write your own Iterator program interface.
+        //It has next, hasNext, and remove.
+            //Skip the other method in the API.
+
+    //Write a program called Slot Machine.
+        //Slot Machine has a global variable that's an int called coins.
+        //Slot Machine also implements the Iterator interface.
+
+    //Iterate through your coins. Each win gives you 2 coins.
+        //Each loss eats a coin.
+        //Make the odds of winning 20% and the odds of losing 80%.
+        //Run the slot machine until you are broke.
+            //Count how many times you got to play until you were out of money.
+
+    //If done early, make a new program that is exactly the same, except coins are coin type objects.
+        //Reminder - this means 2 programs TOTAL if this is done.
+
+    boolean hasNext();
+    int next();
+    void remove();
+}
