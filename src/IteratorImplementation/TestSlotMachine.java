dfe8940883e954d1e4a83315e1a8ff6286da2048
CybErr404 <wattsm6@go.stockton.edu>
2024-10-14 16:49:48 -0400

    Added SlotMachine tester

---
+++ src/IteratorImplementation/TestSlotMachine.java
@@ -0,0 +1,8 @@
+package IteratorImplementation;
+
+public class TestSlotMachine {
+    public static void main(String[] args) {
+        SlotMachine slotMachine = new SlotMachine();
+        slotMachine.run();
+    }
+}
