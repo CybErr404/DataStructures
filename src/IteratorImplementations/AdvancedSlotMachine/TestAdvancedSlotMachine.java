1f024cf83487e71d5997ea48632eb0ce068858e5
CybErr404 <wattsm6@go.stockton.edu>
2024-10-16 17:07:28 -0400

    Finished tester

--- src/IteratorImplementations/AdvancedSlotMachine/TestAdvancedSlotMachine.java
+++ src/IteratorImplementations/AdvancedSlotMachine/TestAdvancedSlotMachine.java
@@ -2,6 +2,7 @@ package IteratorImplementations.AdvancedSlotMachine;
 
 public class TestAdvancedSlotMachine {
     public static void main(String[] args) {
-
+        AdvancedSlotMachine slotMachine = new AdvancedSlotMachine();
+        slotMachine.run();
     }
 }
