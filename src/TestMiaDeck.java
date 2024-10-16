1e37a8a9293582c2a69e69430acbbc1d7e1ea24a
CybErr404 <wattsm6@go.stockton.edu>
2024-10-16 16:49:13 -0400

    Added Stack tester

---
+++ src/TestMiaDeck.java
@@ -0,0 +1,18 @@
+public class TestMiaDeck {
+    public static void main(String[] args) {
+        MiaDeck myDeck = new MiaDeck();
+        System.out.println("Adding 1, 2, and 3: ");
+        myDeck.push(1);
+        myDeck.push(2);
+        myDeck.push(3);
+        System.out.println(myDeck);
+        System.out.println("\nRemoving number on top of the deck: ");
+        myDeck.pop();
+        System.out.println(myDeck);
+        System.out.println("\nChecking the element on the top of the deck: ");
+        System.out.println(myDeck.peek());
+        System.out.println("\nIs the deck empty?\n" + myDeck.empty());
+        System.out.println("\nPosition where 2 is in the deck: \n" + myDeck.search(2));
+        System.out.println("Printing the deck:\n" + myDeck);
+    }
+}
