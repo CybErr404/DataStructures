2e9e28e3f8dc3285739c53b2f3093e2990a0a964
CybErr404 <wattsm6@go.stockton.edu>
2024-10-16 16:48:52 -0400

    Added Stack implementation

---
+++ src/MiaDeck.java
@@ -0,0 +1,64 @@
+//REPRESENTS A STACK DATA STRUCTURE.
+public class MiaDeck {
+    private int[] deck;
+    private int currentPosition;
+
+    public MiaDeck() {
+        deck = new int[10];
+        currentPosition = 0;
+    }
+
+    public boolean empty() {
+        return currentPosition == 0;
+    }
+
+    public void increaseSize() {
+        if(currentPosition == deck.length) {
+            int[] temp = new int[deck.length * 2];
+            for(int i = 0; i < deck.length; i++) {
+                temp[i] = deck[i];
+            }
+            deck = temp;
+        }
+    }
+
+    public int peek() {
+        return deck[currentPosition - 1];
+    }
+
+    public void push(int newData) {
+        if(currentPosition > deck.length) {
+            increaseSize();
+        }
+        deck[currentPosition] = newData;
+        currentPosition++;
+    }
+
+    public int pop() {
+        int temp = deck[currentPosition];
+        currentPosition--;
+        return temp;
+    }
+
+    public int search(int data) {
+        for(int i = 0; i < deck.length; i++) {
+            if(deck[i] == data) {
+                return i - 1;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = currentPosition - 1; i >= 0; i--) {
+            result = result + deck[i] + " ";
+        }
+        return result;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+}
