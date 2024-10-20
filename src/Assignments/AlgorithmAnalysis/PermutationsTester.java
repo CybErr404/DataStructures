6d595d5c1707b9ce9ac5ed994e6c1c088c2d5a72
CybErr404 <wattsm6@go.stockton.edu>
2024-10-20 12:16:26 -0400

    Added permutations files

---
+++ src/Assignments/AlgorithmAnalysis/PermutationsTester.java
@@ -0,0 +1,17 @@
+package Assignments.AlgorithmAnalysis;
+
+/**
+ *  This class measures the performance of the permutation algorithms
+ */
+public class PermutationsTester
+{
+    public static void main(String[] arg)
+    {
+        Permutations p = new Permutations();
+        int[] perm = new int[6];
+        p.algorithm1(perm);
+        p.algorithm2(perm);
+        p.algorithm3(perm);
+    }
+
+}
