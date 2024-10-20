62d19fe215364c70d8dacc6cf24cf9db605411c2
CybErr404 <wattsm6@go.stockton.edu>
2024-10-20 14:22:52 -0400

    Finished base tester class

--- src/Projects/Project1/TestUltimaHalloweenEdition.java
+++ src/Projects/Project1/TestUltimaHalloweenEdition.java
@@ -2,6 +2,7 @@ package Projects.Project1;
 
 public class TestUltimaHalloweenEdition {
     public static void main(String[] args) {
-
+        UltimaHalloweenEdition game = new UltimaHalloweenEdition();
+        //game.run();
     }
 }
