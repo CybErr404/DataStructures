56bc9217f434fb0baf43c883395e0f9018b4d91c
CybErr404 <wattsm6@go.stockton.edu>
2024-10-21 17:14:56 -0400

    Finished PalindromeChecker tester class

--- src/TestPalindromeChecker.java
+++ src/TestPalindromeChecker.java
@@ -3,5 +3,6 @@ public class TestPalindromeChecker {
         PalindromeChecker checker = new PalindromeChecker();
         System.out.println("'hello' is a palindrome: " + checker.isPalindrome2("hello"));
         System.out.println("'racecar' is a palindrome: " + checker.isPalindrome2("racecar"));
+        System.out.println("'ada' is a palindrome: " + checker.isPalindrome2("ada"));
     }
 }
