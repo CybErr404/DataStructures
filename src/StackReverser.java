ad2a48fd69eb6de59ea6f5eafb4959af5409d246
CybErr404 <wattsm6@go.stockton.edu>
2024-10-21 16:43:50 -0400

    Finished StackReverser

---
+++ src/StackReverser.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class StackReverser {
+    private Stack<String> stack;
+
+    public StackReverser() {
+        stack = new Stack<>();
+    }
+
+    public void reverse(String word) {
+        String result = "";
+        for(int i = 0; i < word.length(); i++) {
+            stack.push(String.valueOf(word.charAt(i)));
+        }
+        for(int i = 0; i < word.length(); i++) {
+            result = result + stack.pop();
+        }
+        System.out.println("Original word: " + word + "\n" + "Reversed word: " + result);
+    }
+}
---
+++ src/TestStackReverser.java
@@ -0,0 +1,6 @@
+public class TestStackReverser {
+    public static void main(String[] args) {
+        StackReverser stackReverser = new StackReverser();
+        stackReverser.reverse("abc");
+    }
+}
