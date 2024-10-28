b8b578d950ebd2f96a4767f5898a65064ca98f5e
CybErr404 <wattsm6@go.stockton.edu>
2024-10-28 16:44:52 -0400

    Started main game class

--- src/Projects/Project1/UltimaHalloweenEdition.java
+++ src/Projects/Project1/UltimaHalloweenEdition.java
@@ -1,4 +1,19 @@
 package Projects.Project1;
 
+import DataStructureImplementations.LinkedList.CircularlyLinkedList.BoxCLL;
+import DataStructureImplementations.LinkedList.CircularlyLinkedList.MiaDaisyChainConnectorCLL;
 public class UltimaHalloweenEdition {
+    private MiaDaisyChainConnectorCLL rooms;
+
+    public UltimaHalloweenEdition() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void run() {
+        startGame();
+    }
 }
