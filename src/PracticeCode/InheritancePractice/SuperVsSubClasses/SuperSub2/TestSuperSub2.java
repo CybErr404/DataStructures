dfdebf1698f60c8e592443405312abf2b5ec6d3c
CybErr404 <wattsm6@go.stockton.edu>
2024-11-11 18:15:01 -0500

    Finished SuperSub2

---
+++ src/PracticeCode/InheritancePractice/SuperVsSubClasses/SuperSub2/TestSuperSub2.java
@@ -0,0 +1,7 @@
+package PracticeCode.InheritancePractice.SuperVsSubClasses.SuperSub2;
+
+public class TestSuperSub2 {
+    public static void main(String[] args) {
+        SubClass2 obj = new SubClass2();
+    }
+}
