f3f17eedee019c627a96278aed7daceee58905ee
CybErr404 <wattsm6@go.stockton.edu>
2024-11-11 18:15:14 -0500

    Finished SuperSub3

---
+++ src/PracticeCode/InheritancePractice/SuperVsSubClasses/SuperSub3/SubClass3.java
@@ -0,0 +1,12 @@
+package PracticeCode.InheritancePractice.SuperVsSubClasses.SuperSub3;
+
+public class SubClass3 extends SuperClass3 {
+    @Override
+    public void showValue(int arg) {
+        System.out.println("SUBCLASS: The int argument was " + arg);
+    }
+
+    public void showValue(double arg) {
+        System.out.println("SUBCLASS: The double argument was " + arg);
+    }
+}
---
+++ src/PracticeCode/InheritancePractice/SuperVsSubClasses/SuperSub3/TestSuperSub3.java
@@ -0,0 +1,10 @@
+package PracticeCode.InheritancePractice.SuperVsSubClasses.SuperSub3;
+
+public class TestSuperSub3 {
+    public static void main(String[] args) {
+        SubClass3 myObject = new SubClass3();
+        myObject.showValue(10);
+        myObject.showValue(1.2);
+        myObject.showValue("Hello");
+    }
+}
