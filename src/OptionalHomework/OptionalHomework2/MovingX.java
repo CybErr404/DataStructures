cf923fc6ceabd224473dcbefeb03b08b13215001
CybErr404 <wattsm6@go.stockton.edu>
2024-11-14 20:56:33 -0500

    Started MovingX homework

--- src/OptionalHomework/OptionalHomework2/MovingX.java
+++ src/OptionalHomework/OptionalHomework2/MovingX.java
@@ -1,5 +1,48 @@
 package OptionalHomework.OptionalHomework2;
 
 public class MovingX {
+    private int rowsN;
+    private int columnsM;
 
+    public MovingX() {
+        rowsN = 0;
+        columnsM = 0;
+    }
+
+    public MovingX(int rowAmount, int columnAmount) {
+        rowsN = rowAmount;
+        columnsM = columnAmount;
+    }
+
+    public void forLoopGrid() {
+        int x = 0;
+        int y = 0;
+        System.out.println("Running the forLoopGrid() method: ");
+        if (rowsN == 0 && columnsM == 0) {
+            System.out.println("Please enter the number of rows and columns into the constructor before continuing.");
+        }
+        for(int numTimes = 0; numTimes < (rowsN * columnsM); numTimes++) {
+            for (int i = 0; i < rowsN; i++) {
+                for (int j = 0; j < columnsM; j++) {
+                    if (i == x && j == y) {
+                        System.out.print("[X]");
+                    } else {
+                        System.out.print("[ ]");
+                    }
+                }
+                System.out.println();
+            }
+            if (x < rowsN) {
+                x++;
+            } else {
+                x = 0;
+                y++;
+            }
+            System.out.println();
+        }
+    }
+
+    public void arrayGrid() {
+        System.out.println("Running the arrayGrid() method: ");
+    }
 }
---
+++ src/OptionalHomework/OptionalHomework2/TestMovingX.java
@@ -0,0 +1,9 @@
+package OptionalHomework.OptionalHomework2;
+
+public class TestMovingX {
+    public static void main(String[] args) {
+        MovingX test = new MovingX(3, 3);
+        test.forLoopGrid();
+        test.arrayGrid();
+    }
+}
