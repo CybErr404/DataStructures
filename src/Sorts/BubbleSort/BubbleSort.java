a311221ac5661d1c20dabf5a4c90872c330f466c
CybErr404 <wattsm6@go.stockton.edu>
2024-11-14 21:09:24 -0500

    Started Bubble Sort

--- src/Sorts/BubbleSort/BubbleSort.java
+++ src/Sorts/BubbleSort/BubbleSort.java
@@ -1,6 +1,15 @@
 package Sorts.BubbleSort;
 
+import java.util.ArrayList;
+
 public class BubbleSort {
+    private ArrayList<Integer> data;
+    public BubbleSort() {
+        data = new ArrayList<>();
+    }
+    public void generateArray(ArrayList<Integer> dataArray) {
+
+    }
     public void bubbleSort() {
 
     }
