7b6a8c382090be63c31699c29ae51eb0483b5db6
CybErr404 <wattsm6@go.stockton.edu>
2024-11-14 21:05:24 -0500

    Finished selection sort

--- src/Sorts/SelectionSort/SelectionSort.java
+++ src/Sorts/SelectionSort/SelectionSort.java
@@ -1,4 +1,25 @@
 package Sorts.SelectionSort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public void selectionSort(int[] elements) {
+        int stepCounter = 0;
+        System.out.println("Original list: " + Arrays.toString(elements));
+        for (int j = 0; j < elements.length - 1; j++) {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++) {
+                if (elements[k] < elements[minIndex]) {
+                    minIndex = k;
+                }
+            }
+            if (j != minIndex) {
+                int temp = elements[j];
+                elements[j] = elements[minIndex];
+                elements[minIndex] = temp;
+                stepCounter++;
+            }
+        }
+        System.out.println("Amount of steps: " + stepCounter + "\nSorted list: " + Arrays.toString(elements));
+    }
 }
--- src/Sorts/SelectionSort/TestSelectionSort.java
+++ src/Sorts/SelectionSort/TestSelectionSort.java
@@ -1,4 +1,9 @@
 package Sorts.SelectionSort;
 
 public class TestSelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {30, 40, 10, 50, 20};
+        SelectionSort test = new SelectionSort();
+        test.selectionSort(arr);
+    }
 }
